0ead70afde67a07fa261f0a4fcf8964c018e7490
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2020-03-28 21:26:42 +0000

    retabbing

--- src/main.cpp
+++ src/main.cpp
@@ -29,54 +29,54 @@ MovementController movementCtrl(
 );
 
 void CtrlCHandler(int s) {
-	printf("Caught signal %d\n", s);
+    printf("Caught signal %d\n", s);
     movementCtrl.StopAll();
     movementCtrl.ResetPWM();
-	exit(1);
+    exit(1);
 }
 
 int main(int argc, const char** argv) {
-	// Initializing signals
-	struct sigaction sigIntHandler;
+    // Initializing signals
+    struct sigaction sigIntHandler;
 
-	sigIntHandler.sa_handler = CtrlCHandler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
+    sigIntHandler.sa_handler = CtrlCHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
 
-	sigaction(SIGINT, &sigIntHandler, NULL);
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
-	std::cout << "Setting up" << std::endl;
+    std::cout << "Setting up" << std::endl;
 
-	// Initializing joystick
-	Joystick joystick("/dev/input/js0");
-	if (!joystick.isFound()) {
-		printf("Joy stick not found.\n");
-		exit(1);
-	}	
+    // Initializing joystick
+    Joystick joystick("/dev/input/js0");
+    if (!joystick.isFound()) {
+        printf("Joy stick not found.\n");
+        exit(1);
+    }   
 
-	for (;;) {
-		delayMicroseconds(1000);
-		JoystickEvent event;
-		if (joystick.sample(&event)) {
-			if (event.isButton()) {
-				switch (event.number) {
-				case 13:
-					if (event.value == 0) {
+    for (;;) {
+        delayMicroseconds(1000);
+        JoystickEvent event;
+        if (joystick.sample(&event)) {
+            if (event.isButton()) {
+                switch (event.number) {
+                case 13:
+                    if (event.value == 0) {
                         movementCtrl.StopAll();
-					} else {
+                    } else {
                         movementCtrl.MoveForward(4096);
-					}
-					break;
-				case 14:
-					if (event.value == 0) {
-						// Key up
-					} else {
-						// Key Down
-					}
-					break;
-				}
-			}
-		}
-	}
-	return 0;
+                    }
+                    break;
+                case 14:
+                    if (event.value == 0) {
+                        // Key up
+                    } else {
+                        // Key Down
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
 }
